1c0bc573235c23b881dea91fc67ae8cc1ecd84dc
Stephen-Alex-Ngo <stephen.ngo@orangeandbronze.com>
2023-09-18 23:47:29 +0800

    Main class changed

--- UVAprojectsJAVA/Main.java
+++ UVAprojectsJAVA/Main.java
@@ -3,11 +3,7 @@ import java.util.Scanner;
 public class Main
 {
     public static void main(String [] args)
-    { 
-       while(true)
-       {
-           System.out.println("");
-       }
-      
+    {
+           System.out.println("Hello World");
     }
 }
